7da876f69fef59adc66d1bbb902a03b872d8ee3b
Muhammad Haekal <93haekal@gmail.com>
2018-09-02 01:55:30 +0700

    Add Simple State

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      counter : 0
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        State : {this.state.counter}
       </div>
     );
   }
