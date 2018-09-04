e1b9c9b1e6ce07f9ea5892277a13313f4593e6e1
Muhammad Haekal <93haekal@gmail.com>
2018-09-04 14:29:04 +0700

    Save data from api to state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3000/employees").then(res =>
+      this.setState({
+        employees: res.data
+      })
     );
+  }
+
+  render() {
+    return <div>Hello React + API</div>;
   }
 }
 
