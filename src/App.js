c048ee07a0daf7d794150b1b938827c2a95e9899
Muhammad Haekal <93haekal@gmail.com>
2018-09-04 07:51:15 +0700

    Simple Routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import Home from "./Home";
 import About from "./About";
 import Topics from "./Topics";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-        <About />
-        <Topics />
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>|
+            <Link to="/about">About</Link>|
+            <Link to="/topics">Topics</Link>
+          </nav>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
     );
   }
 }
