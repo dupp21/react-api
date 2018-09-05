2fdfc07fb66163a75c0fa1ac89080dd3d8141cfa
Muhammad Haekal <93haekal@gmail.com>
2018-09-05 11:33:00 +0700

    insert data from api to state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentWillMount = async () => {
+    axios.get("http://localhost:3000/todos").then(res => {
+      this.setState({
+        todos: res.data
+      });
+    });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">React + API</div>;
   }
 }
 
