7cdc64979ae2b1da51f75f991cf8fc21c26a031f
Muhammad Haekal <93haekal@gmail.com>
2018-09-07 00:31:54 +0700

    add private route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import Protected from "./Protected";
+import PrivateRoute from "./PrivateRoute";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/public">Home Page</Link> |{" "}
+            <Link to="/products">Products Page</Link> | {" "}
+          </nav>
+
+          {/* <Route path="/public" component={Public} />
+          <Route path="/login" component={Login} /> */}
+          <PrivateRoute path="/protected" component={Protected} name="budi" />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect,
+    withRouter
+  } from "react-router-dom";
+  
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      props.isAuthenticated ? (
+        <Component {...props} {...rest} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default PrivateRoute;
