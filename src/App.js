033dae5785539840ff3cf6500f30084485df58c1
Muhammad Haekal <93haekal@gmail.com>
2018-09-07 01:45:07 +0700

    Finish Login And Redirect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import {
   BrowserRouter as Router,
@@ -10,20 +9,59 @@ import {
 } from "react-router-dom";
 import Protected from "./Protected";
 import PrivateRoute from "./PrivateRoute";
+import Home from "./Home";
+import Login from "./Login";
+import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isAuthenticated: false
+    };
+  }
+
+  login = (email, password) => {
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/accounts/login`, {
+        email: email,
+        password: password
+      })
+      .then(async res => {
+        localStorage.token = res.data.token;
+        await this.setState({
+          isAuthenticated: true
+        });
+      });
+  };
+
   render() {
     return (
       <Router>
         <div>
-          <nav>
-            <Link to="/public">Home Page</Link> |{" "}
-            <Link to="/products">Products Page</Link> | {" "}
+          <nav className="nav-bar">
+            <div>
+              <Link to="/">Home Page</Link> |{" "}
+              <Link to="/employees">Employees Data</Link>
+            </div>
+            {this.state.isAuthenticated ? (
+              <div>ProfPict</div>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
           </nav>
+          <Route exact path="/" component={Home} />
 
-          {/* <Route path="/public" component={Public} />
-          <Route path="/login" component={Login} /> */}
-          <PrivateRoute path="/protected" component={Protected} name="budi" />
+          <Route
+            path="/login"
+            render={props => (
+              <Login
+                isAuthenticated={this.state.isAuthenticated}
+                {...props}
+              />
+            )}
+          />
+          <PrivateRoute path="/employees" component={Protected} />
         </div>
       </Router>
     );
---
+++ src/Login.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input_email: "admin@admin.com",
+      input_password: "admin"
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.props.isAuthenticated) {
+      return <Redirect to='/employees' />
+    }
+  }
+
+  render() {
+    const { input_email, input_password } = this.state;
+
+    return (
+      <div>
+        {this.renderRedirect()}
+        <h1>Login Page</h1>
+        Email : <br />
+        <input
+          type="text"
+          name="input_email"
+          value={this.state.input_email}
+          onChange={this.handleChange}
+        />
+        <br />
+        Password : <br />
+        <input
+          type="pasword"
+          name="input_password"
+          value={this.state.input_password}
+          onChange={this.handleChange}
+        />
+        <br />
+        <button onClick={() => this.props.login(input_email, input_password)}>
+          Login
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Login;
