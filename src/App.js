1b7228c3b1f66aa4e474338870626c8d55a22fa7
Muhammad Haekal <93haekal@gmail.com>
2018-09-07 10:19:09 +0700

    Create Router Link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Router>
+      <div>
+        <nav className="nav-bar">
+          <div>
+            <Link to="/">Home</Link> | <Link to="/employees">Employees</Link>
+          </div>
+          {this.state.isAuthenticated ? (
+            <div>
+              Welcome !!!,<button onClick={() => this.logout()}>Logout</button>
+            </div>
+          ) : (
+            <Link to="/login">Login </Link>
+          )}
+        </nav>
+        <hr />
       </div>
+    </Router>
     );
   }
 }
